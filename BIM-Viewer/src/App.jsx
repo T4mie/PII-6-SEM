a78a26b047a68fa5b5cd35dab2c147bc8bdf60db
Uper05 <jvfdessani@gmail.com>
2025-10-14 19:56:06 -0300

    Roteamento de Telas e Upload_Screen Modficada

--- BIM-Viewer/src/App.jsx
+++ BIM-Viewer/src/App.jsx
@@ -1,13 +1,17 @@
 // src/App.jsx
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import Upload_Screen from "./components/Upload_Screen";
 import Viewer from "./components/Viewer";
 import "./assets/css/viewer.css";
 
 function App() {
   return (
-    <div className="App">
-      <Viewer />
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Upload_Screen />} />
+          <Route path="/viewer" element={<Viewer />} />
+        </Routes>
+    </BrowserRouter>
   );
 }
 
--- BIM-Viewer/src/components/Upload_Screen.jsx
+++ BIM-Viewer/src/components/Upload_Screen.jsx
@@ -1,4 +1,6 @@
+import { Link, Route } from 'react-router-dom'
 import '../assets/css/upload_screen.css'
+import { FaFileUpload } from "react-icons/fa";
 
 export default function Upload_Screen() {
 
@@ -7,11 +9,18 @@ export default function Upload_Screen() {
         <div className="container">
             <div className='divisor'></div>
             <div className='collum'>
-
+                <h2>Insira o modelo da construção</h2>
+                
+                    <div>
+                        <FaFileUpload size={48} color='grey'/>
+                    </div>
             </div>
             <div className='divisor'></div>
             <div className='collum'>
-
+                <h2>Insira a foto da construção</h2>
+                <div>
+                    <FaFileUpload size={48} color='grey'/>
+                </div>
             </div>
             <div className='divisor'></div>
         </div>
